de8205d6ee345678f4d845cd7b5c8dec07ef1908
DeroMuli <musembimuli1999@gmail.com>
2022-04-11 09:54:09 +0300

    more familiar to format

---
+++ chapter2/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navigation from './Navigation.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import './App.css';
+
+const App = () => (
+  <Navigation/>
+);
+
+export default App;

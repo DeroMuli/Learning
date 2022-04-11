9086fb588884f3c3363fd475a9bbd27a8464f809
DeroMuli <musembimuli1999@gmail.com>
2022-04-11 16:32:20 +0300

    chapter 3 start

--- chapter3/src/App.js
+++ chapter3/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import MessagePanel  from './MessagePanel.js' 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <MessagePanel>
+    <MessagePanel.Heading text='Show/Hide' />
+    <MessagePanel.Content>
+      Phasellus sed velit venenatis, suscipit eros a, laoreet dui.
+    </MessagePanel.Content>
+  </MessagePanel>
   );
 }
 
---
+++ chapter3/src/MessagePanel.js
@@ -0,0 +1,20 @@
+import React from 'react'
+const MessagePanel = class extends React.Component {
+    render() {
+        return ( <div> {this.props.children} </div>)
+    }
+}
+
+MessagePanel.Heading = class extends React.Component{
+    render() {
+        return ( <h2>{this.props.text}</h2> )
+    }
+}
+
+MessagePanel.Content = class extends React.Component{
+    render() {
+        return (<div className='well'> {this.props.children} </div>)
+    }
+}
+
+export default MessagePanel

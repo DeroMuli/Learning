4adc980ae84e0ddbac11ce042b8e887fafa05ff0
DeroMuli <musembimuli1999@gmail.com>
2022-04-15 13:52:06 +0300

    chapter5

---
+++ chapter5/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap';
+import './App.css';
+
+const App = function () {
+  return (
+    <Container>
+    </Container>
+  );
+} 
+
+export default App;
